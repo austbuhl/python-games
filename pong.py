d98a7e5f316baf498c274a57ff758b794010a67b
AB <austinbuhler@gmail.com>
2021-02-05 15:10:46 -0600

    Working + Scoreboard

--- pong.py
+++ pong.py
@@ -6,6 +6,11 @@ wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_1 = 0
+score_2 = 0
+
+# Paddles setup
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape("square")
@@ -22,7 +27,7 @@ paddle_2.shapesize(stretch_wid=5, stretch_len=1)
 paddle_2.penup()
 paddle_2.goto(350, 0)
 
-
+# Ball setup
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
@@ -32,7 +37,14 @@ ball.goto(0, 0)
 ball.dx = 5
 ball.dy = 5
 
-
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player 1: 0  Player 2: 0', align='center', font=('Courier', 24, 'normal'))
 
 def paddle_1_up():
   y = paddle_1.ycor()
@@ -80,11 +92,23 @@ while True:
   if ball.xcor() > 390:
     ball.goto(0, 0)
     ball.dx *= -1
+    score_1 += 1
+    pen.clear()
+    pen.write('Player 1: {}  Player 2: {}'.format(score_1, score_2), align='center', font=('Courier', 24, 'normal'))
+
   
   if ball.xcor() < -390:
     ball.goto(0, 0)
     ball.dx *= -1
+    score_2 += 1
+    pen.clear()
+    pen.write('Player 1: {}  Player 2: {}'.format(score_1, score_2), align='center', font=('Courier', 24, 'normal'))
 
   # paddle and ball collision
-  if ball.xcor() > 340 and (ball.ycor() < paddle_2.ycor() + 50 and ball.ycor() > paddle_2.ycor() - 50):
+  if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_2.ycor() + 40 and ball.ycor() > paddle_2.ycor() - 40):
+    ball.setx(340)
+    ball.dx *= -1
+
+  if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_1.ycor() + 40 and ball.ycor() > paddle_1.ycor() - 40):
+    ball.setx(-340)
     ball.dx *= -1
