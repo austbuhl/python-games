d0aa39e08498760227fda946dc7b3678989b0551
AB <austinbuhler@gmail.com>
2021-02-09 15:42:16 -0600

    Converting shape formats

--- tetris.py
+++ tetris.py
@@ -156,13 +156,36 @@ def create_grid(locked_positions={}):
   return grid
 
 def convert_shape_format(shape):
-  pass
+  positions = []
+  shape_format = shape.shape[shape.rotation % len(shape.shape)]
+
+  for i, line in enumerate(shape_format):
+    row = list(line)
+    for j, col in enumerate(row):
+      if col == '0':
+        positions.append((shape.x + j, shape.y + i))
+  
+  for i, pos in enumerate(positions):
+    positions[i] = (pos[0] - 2, pos[1] - 4)
 
 def valid_space(shape, grid):
-  pass
+  accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
+  ## flattens the list - [[(0, 1)], [(2, 3)]] -> [(0, 1), (2, 3)]
+  accepted_pos = [j for sub in accepted_pos for j in sub]
+
+  formatted = convert_shape_format(shape)
+  for pos in formatted:
+    if pos not in accepted_pos:
+      if pos[1] > -1:
+        return False
+  return True
 
 def check_lost(positions):
-  pass
+  for pos in positions:
+    x, y = pos
+    if y < 1:
+      return True
+  return False
 
 def get_shape():
   return Piece(5, 0, random.choice(shapes))
